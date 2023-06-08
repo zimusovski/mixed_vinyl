be598a3d0e81c899c1ed1c5927653502b0a58b45
zimusosvski <zimusovski@gmail.com>
2023-06-08 15:34:46 +0300

    seccode down

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+x
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
